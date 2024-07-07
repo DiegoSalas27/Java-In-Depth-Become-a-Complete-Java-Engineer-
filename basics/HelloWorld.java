1cf5454bc2752dd2f38c8c968b14afe568d35fc3
DiegoSalas27 <dominicsc2hs@gmail.com>
2024-07-07 13:57:07 -0400

    Writing First Java Program

---
+++ basics/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!!");
+	}
+}
+
+class GoodBye {
+	public static void main(String[] args) {
+		System.out.println("Goodbye!!");
+	}
+} 
