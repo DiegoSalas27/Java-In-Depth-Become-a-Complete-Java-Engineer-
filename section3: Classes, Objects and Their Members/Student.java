d76f3271580f90c8d5df0a45af4e6953520cdd35
DiegoSalas27 <dominicsc2hs@gmail.com>
2024-07-20 23:29:28 -0400

    Local, Global, and Static variables

--- section3: Classes, Objects and Their Members/Student.java
+++ section3: Classes, Objects and Their Members/Student.java
@@ -20,10 +20,14 @@ public class Student {
 
     char degreeUnicode = '\u0042'; // unicode representation in hexadecimal (UTF 16) <- also called code point
 
-    char happyFace = '\u263A';
+    char happyFace = '\u263A'; // global variable
+
+    // static variables are specific to the class.
+    public static int computeCount;
 
     void compute() {
-        int nextId = id + 1;
+        computeCount += 1;
+        int nextId = id + 1; // local variable
 
         System.out.println("id: " + id);
         System.out.println("nextId: " + nextId);
@@ -36,10 +40,19 @@ public class Student {
         System.out.println("degree: " + degree);
         System.out.println("degreeUnicode: " + degreeUnicode);
         System.out.println("happyFace: " + happyFace);
+        System.out.println("Student.computeCount: " + computeCount + "\n");
     }
 
     public static void main(String[] args) {
-        Student s = new Student();
-        s.compute();
+        Student s1 = new Student();
+        s1.compute(); // computeCount = 1
+        Student s2 = new Student();
+        s2.compute(); // computeCount = 2
+        Student s3 = new Student();
+        s3.compute(); // computeCount = 3
+        System.out.println("Students1.computeCount: " + s1.computeCount + "\n");
+        System.out.println("Student2.computeCount: " + s2.computeCount + "\n");
+        System.out.println("Student3.computeCount: " + s3.computeCount + "\n");
+
     }
 }
