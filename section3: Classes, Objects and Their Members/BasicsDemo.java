5b6641ccf6edfcb21d0eb324d258505403d77cc0
DiegoSalas27 <dominicsc2hs@gmail.com>
2024-07-20 21:20:17 -0400

    Primivite types test

--- section3: Classes, Objects and Their Members/BasicsDemo.java
+++ section3: Classes, Objects and Their Members/BasicsDemo.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 class BasicsDemo {
   // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
   static void print() {
@@ -9,9 +11,36 @@ class BasicsDemo {
     System.out.print(" ");                // Print a space
     System.out.print("world!!");
   }
+
+  static void primitives() {
+    System.out.println("\n\nInside primitives ...");
+    int intHex = 0x0041; // Hexadecimal format 0 - 9 A - F/ 167 power 0 * 1 + 16 power 1 * 4
+    System.out.println("intHext: " + intHex);
+
+    // hexadecimal are useful to perform bitwise operations. Although binary numbers better. Binary operations
+    // are only possible since Java 7.
+
+    int intBinary = 0b01000001; // 65
+    System.out.println("intBinary: " + intBinary);
+  }
   
   public static void main(String[] args) {	
 	   	// Language Basics 1
-		print();				
-  }       
+//		print();
+//        primitives();
+    System.out.println(1 - 0.9);
+    System.out.println(0.1 + 0.2);
+
+    double price = 1000;
+    double discountPercent = 0.9;
+    double discountAmount = price * discountPercent;
+    System.out.println(price - discountAmount);
+    System.out.println(price * (1 - discountAmount)); // 0.1 cannot be accurately represented.
+    // when working with money, don't use double or float, use BigDecimal.
+
+    // avoid float and double if exact numbers are required.
+    BigDecimal first = new BigDecimal("0.1");
+    BigDecimal second = new BigDecimal("0.2");
+    System.out.println(first.add(second));
+  }
 }
--- section3: Classes, Objects and Their Members/Student.java
+++ section3: Classes, Objects and Their Members/Student.java
@@ -1,11 +1,41 @@
 public class Student {
     int id = 1000;
 
+    byte age = 18;
+
+    // byte rank = 165; //[-128, 127] possible lossy conversion from int to byte
+
+    short rank = 165;
+    long phone = 223_456_7890L; // Java 7 onwards -- readability
+
+    // Integer literals: int literal, long literal
+    int minValue = Integer.MIN_VALUE; // Boxed Primitive class
+    int maxValue = Integer.MAX_VALUE;
+
+    float gpa = 3.8f; // 3.8 (double) / 3.8f (float)
+
+    double gpa2 = 3.8e4; // scientific notation
+
+    char degree = 'B';
+
+    char degreeUnicode = '\u0042'; // unicode representation in hexadecimal (UTF 16) <- also called code point
+
+    char happyFace = '\u263A';
+
     void compute() {
         int nextId = id + 1;
 
         System.out.println("id: " + id);
         System.out.println("nextId: " + nextId);
+        System.out.println("age: " + age);
+        System.out.println("phone: " + phone);
+        System.out.println("min int value : " + minValue);
+        System.out.println("max int value: " + maxValue);
+        System.out.println("gpa: " + gpa);
+        System.out.println("gpa2: " + gpa2);
+        System.out.println("degree: " + degree);
+        System.out.println("degreeUnicode: " + degreeUnicode);
+        System.out.println("happyFace: " + happyFace);
     }
 
     public static void main(String[] args) {
